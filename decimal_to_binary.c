ed5bad96e2f986c9cea115b866b5efff3f57cbde
Yash Mishra <yash.48725095@gmail.com>
2024-09-01 19:19:57 +0530

    decimal_to_binary completed

--- decimal_to_binary.c
+++ decimal_to_binary.c
@@ -1,22 +1,26 @@
+/*Yash Mishra
+Decimal to Binary*/
+
 #include <stdio.h>
 
 
 int decitobin(int number)
 {
-    int i =0;
+    int j =0;
     int lst[32];
-    int quo;
-    while(quo>0)
+    while(number>0)
     {
-        quo = number/2;
-        lst[i] = number%2;
-        i++;
+        lst[j] = number%2;
+        number = number/2;
+        j++;
     }
     printf("In Binary- ");
-    for(i; i>0; i--)
+    //We need to start the loop from j - 1 as otherwise it prints bogus values from the index outside of the desired range :(
+    for(int i = j-1; i>=0; i--)
     {
         printf("%d",lst[i]);
     }
+    printf("\n");
     return 0;
 }
 
