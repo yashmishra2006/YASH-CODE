a5e4f14e2edb07c020dd3ab53ecc3b667b654a64
Yash Mishra <yash.48725095@gmail.com>
2024-09-28 17:49:09 +0000

    Sep 28, 2024, 11:19 PM

--- unique.c
+++ unique.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
+    #include <stdio.h>
 
-int main(void){
-int num[3] = {3,4,5};
-int unique[3] = {0,0,0};
-int count = 0;
+    int main(void){
+    int num[3] = {7,4,5};
+    int unique[3] = {0,0,0};
+    int count = 0;
 
-for(int i=0; i<3; i++)
-{
-    for(int j=0; j<count; j++)
+    for(int i=0; i<3; i++)
     {
-        if(unique[j] == i)
+        for(int j=0; j<count; j++)
         {
-            break;
+            if(unique[j] == i)
+            {
+                break;
+            }
         }
-    }
 
-    unique[count+1] = i;    
-    count++;
-}
-for(int i=0; i<3; i++)
-{
-    //printf("%d\n", unique[i]);
-    printf("%d\n", num[i]);
-}
-}
+        unique[count+1] = i;    
+        count++;
+    }
+    for(int i=0; i<3; i++)
+    {
+        //printf("%d\n", unique[i]);
+        printf("%d\n", num[i]);
+    }
+    }
