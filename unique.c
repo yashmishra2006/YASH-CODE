a980b8e0d711be4a7f5235420159beaa319dda44
Yash Mishra <yash.48725095@gmail.com>
2024-09-28 18:42:21 +0000

    Sep 29, 2024, 12:12 AM

--- unique.c
+++ unique.c
@@ -1,27 +1,47 @@
-    #include <stdio.h>
+#include <stdio.h>
 
-    int main(void){
-    int num[3] = {1,1,2};
-    int unique[3] = {0,0,0};
-    int count = 1;
+int removeDuplicates(int arr[], int n) {
+    int temp[n]; // Temporary array to store unique elements
+    int j = 0;   // Index for the unique array
 
-    for(int i=0; i<3; i++)
-    {
-        for(int j=0; j<count; j++)
-        {
-            if(unique[j] == num[i])
-            {
-                continue;
+    // Iterate through the original array
+    for (int i = 0; i < n; i++) {
+        int isDuplicate = 0;
+
+        // Check if the element is already in the unique array
+        for (int k = 0; k < j; k++) {
+            if (arr[i] == temp[k]) {
+                isDuplicate = 1;
+                break;
             }
         }
 
-        unique[count] = i;    
-        count++;
+        // If not a duplicate, add to the unique array
+        if (!isDuplicate) {
+            temp[j] = arr[i];
+            j++;
+        }
     }
-    for(int i=0; i<3; i++)
-    {
-        //printf("%d\n", unique[i]);
-        printf("%d\n", num[i]);
+    
+printf("%d\n", j-1);
+
+}
+
+    
+
+
+int main() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
-    printf("num 0 - %d\n", num[0]);
-    }
+
+    removeDuplicates(arr, n);
+
+    return 0;
+}
