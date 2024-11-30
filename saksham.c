bc1147ca887e678904dd6bee7b6a4c9f43667546
YASH MISHRA <yash.48725095@gmail.com>
2024-11-30 11:23:43 +0000

    Nov 30, 2024, 4:53 PM

---
+++ saksham.c
@@ -0,0 +1,18 @@
+int main() {
+    int n;
+
+    printf("Enter the number of elements in the array: ");
+    scanf("%d", &n);
+
+    char arr[n];
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf(" %c", &arr[i]);
+    }
+
+    for(int i=0; i<n; i++){
+        
+    }
+
+}
