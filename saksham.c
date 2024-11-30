b69aa3ef44e01cf221f2ee25a2c442a3da7f6eae
YASH MISHRA <yash.48725095@gmail.com>
2024-11-30 11:34:22 +0000

    Nov 30, 2024, 5:04 PM

--- saksham.c
+++ saksham.c
@@ -5,7 +5,8 @@ int main() {
     scanf("%d", &n);
 
     char arr[n];
-    bool list[95];
+    int list[95];
+     logic;
 
     printf("Enter %d elements:\n", n);
     for (int i = 0; i < n; i++) {
@@ -13,9 +14,21 @@ int main() {
     }
 
     for(int i=0; i<n; i++){
-        
-
-
+        if(arr[i]!=arr[i+1]){
+            list[arr[i+1]-32]++;
+        }
     }
+
+    for(int i=0; i<95; i++){
+        if(arr[i]>1){
+            logic = true;
+            break;
+        }
+    }
+
+    if(logic==true){
+        printf("No");
+    }
+    else{printf("Yes");}
 
 }
