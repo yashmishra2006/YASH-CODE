83afe9c52c9bba8750710ccd2fe707d3b7a8e230
YASH MISHRA <yash.48725095@gmail.com>
2024-12-12 17:16:34 +0000

    Dec 12, 2024, 10:46 PM

--- USICT/USICT/meme.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    for(;;)
-    {
-        printf("+1");
-    }
-}
--- USICT/mainpdf/assi1/aux001.h
+++
@@ -1,7 +0,0 @@
-#ifndef AUX001_H
-#define AUX001_H
-
-double currCpuTime();
-double dblTimeDiff(double endTime, double startTime);
-
-#endif // AUX001_H
