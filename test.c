6fa2173c2e448bcdbcf59c35d6efd47e8707b6c5
YASH MISHRA <yash.48725095@gmail.com>
2024-12-12 11:37:05 +0000

    Dec 12, 2024, 5:07 PM

--- test.c
+++ test.c
@@ -3,20 +3,19 @@
 
 int main(void){
 
-    float l,b,a,lt,bt;
+    float l,b,a;
+    int lt,bt;
 
-    printf("Enter length");
+    printf("Enter length - ");
     scanf("%f", &l);
-    printf("Enter breadth");
+    printf("Enter breadth - ");
     scanf("%f", &b);
-    printf("Enter size of tile");
+    printf("Enter size of tile - ");
     scanf("%f", &a);
 
-    lt = int(ceil(l/a));
+    lt = ceil(l/a);
     b = b-a;
-    bt = int(ceil(b/a));
-    printf("%f", lt);
-    printf("%f", bt);
-    printf("%f", lt*bt);
+    bt = ceil(b/a) + 1;
+    printf("%i", lt*bt);
     return 0;
 }
