876b397519ee417816ad663b1a1bb259f5f84176
YASH MISHRA <yash.48725095@gmail.com>
2024-12-12 11:43:19 +0000

    Dec 12, 2024, 5:13 PM

--- test.c
+++ test.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <math.h>
-
+ 
 int main(void){
-
+ 
     float l,b,a;
     int lt,bt;
-
-    printf("Enter length - ");
-    scanf("%f", &l);
-    printf("Enter breadth - ");
-    scanf("%f", &b);
-    printf("Enter size of tile - ");
-    scanf("%f", &a);
-
+    
+    scanf("%f %f %f", &l,&b,&a);
+ 
     lt = ceil(l/a);
     b = b-a;
     bt = ceil(b/a) + 1;
-    printf("%i", lt*bt);
-    return 0;
+    printf
+    return lt*bt;
 }
